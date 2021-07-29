c6468bc29abd4eff2775dde7d75c4f65e5dc1931
Parsons <gongyingparsons@gmail.com>
2021-07-29 11:45:53 +0800

    feature: 2-31 https://www.notion.so/2-31-mockup-00974bddd8cb45ff99bdf829ca3705ff

--- src/views/Achievement.vue
+++ src/views/Achievement.vue
@@ -1,16 +1,34 @@
 <template>
-    <div class="page-achievement max-width">
-        <div>
-            謎樣的成就系統切版還沒出<br>
-            <img src="https://media.tenor.com/images/2cd1859d8e5b6cfae05e9e33c1be2b80/tenor.gif">
+    <div class="page-achievement max-width charmbo-bgcolor-gray">
+        <div class="px-5 pt-8 text-align-center">
+            <div class="d-flex border-radius-24 charmbo-bgcolor-white pa-1 fw-semi-bold">
+                <div class="flex-grow-1 text-align-center width-50 charmbo-bgcolor-chat border-radius-24 py-2">Match</div>
+                <div class="flex-grow-1 text-align-center width-50 py-2">Star</div>
+            </div>
+            <div class="d-flex justify-center mt-16">
+                <div>
+                    <v-img
+                        :src="require('@/assets/img/programming.png')"
+                        height="293"
+                        width="289"
+                    />
+                </div>
+            </div>
+            <div class="text-align-center fw-black fs-18 my-2">有趣的新功能還在開發中</div>
+            <div class="text-align-center fw-black fs-18 my-2">敬請期待！</div>
+            <charmbo-botton class="mt-10" @click="$router.push({name: 'pair'})">
+                <span class="mx-2">繼續尋找配對對象</span>
+            </charmbo-botton>
         </div>
         <footer-bar activeTab="achievement"></footer-bar>
     </div>
 </template>
 <script>
-    import FooterBar from '../components/Footer.vue';
+    import CharmboBotton from '@/components/CharmboBotton.vue';
+    import FooterBar from '@/components/Footer.vue';
     export default {
         components: {
+            CharmboBotton,
             FooterBar,
         },
     }
